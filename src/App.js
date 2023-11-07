39430e850b030ebbb0a0d5d0c0959bafe6121553
TSantiagoIdalgo51 <tomasidalgo51@gmail.com>
2023-11-07 14:17:31 -0300

    Landing page estilizada

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-
 import { Routes, Route } from "react-router-dom";
-
-import Home from "./Views/Home/Home";
 import Landing  from "./Views/Landing/Landing";
 import FormAdmin from "./Views/FormAdmin/FormAdmin";
-
-
+import NavBar from "./components/NavBar/navbar/NavBar";
 import './App.css';
 
 function App() {
 
   return (
-    <div className="App">
-
+    <div className="app">
+      <NavBar/>
       <Routes>
-        
         <Route path= "/" element={<Landing/>} />
-        <Route path= "/home" element={<Home/>} />
-        <Route path = "/home/admin" element = { <FormAdmin/>} />
-
-
+        <Route path = "/home/admin" element = {<FormAdmin/>}/>
       </Routes>
-
-      
     </div>
   );
 }
