111dfa120a177b0d74dbf377104bc1047a26d69c
TSantiagoIdalgo51 <tomasidalgo51@gmail.com>
2023-11-15 11:10:53 -0300

    filtro por rango de precio

--- src/components/Filters/filterbox/FilterBox.jsx
+++ src/components/Filters/filterbox/FilterBox.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
 import Style from './filterBox.module.css'
 
-const FilterBox = ({ name, filterChange, handleCheck, check, filters}) => {
+const FilterBox = ({ name, filterChange, handleCheck, check, filters,  min, setMin, max, setMax}) => {
     function checked (e) {
         if (e.target.checked) {
             handleCheck({...check, [e.target.name]: true})
@@ -13,13 +13,34 @@ const FilterBox = ({ name, filterChange, handleCheck, check, filters}) => {
     },[filters.length, handleCheck])
     return (
         <div className={Style.box}>
+            {name === 'range'?
+            <MinMax min={min} max={max} setMin={setMin} setMax={setMax}/>
+            :<Fragment>
             <input hidden name={name} type="checkbox" id={name} onChange={filterChange} 
             onClick={checked}/>
             <label htmlFor={name} 
             className={`${Object.keys(check).includes(name) && filters.length > 0
             ? Style.box_label_check 
             : Style.box_label}`}>{name}</label>
+            </Fragment>}
         </div>
+    )
+}
+
+const MinMax = ({ min, max, setMin, setMax }) => {
+    return (
+        <Fragment>
+        <div className={Style.box_range}>
+        <div className={Style.box_range_box}>
+          <label htmlFor="min">Min:</label>
+          <input type="number" id="min" value={min} onChange={e => setMin(e.target.value)}/>
+         </div>
+         <div className={Style.box_range_box}>
+          <label className={Style.box_range_label} htmlFor="max">Max:</label>
+          <input type="number" value={max} id="max" onChange={e => setMax(e.target.value)}/>
+        </div>
+        </div>
+        </Fragment>
     )
 }
 
