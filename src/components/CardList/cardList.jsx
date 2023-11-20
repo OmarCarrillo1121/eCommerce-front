c963fa24312116585223d5d340873ad7d4fa7905
unknown <luis901810@gmail.com>
2023-11-20 09:06:34 -0500

    subo modificacion de la cardlist

--- src/components/CardList/cardList.jsx
+++ src/components/CardList/cardList.jsx
@@ -1,11 +1,12 @@
 import Style from './cardList.module.css'
 import Card from "../Card/Card";
 import { AnimatePresence } from 'framer-motion';
-
+// solo puse que si no es array no muestre nada en vez del error
 const CardList = ({ indexOfLastGame, indexOfFirstGame, currentGames }) => {
     return (
       <AnimatePresence>
         <div className={Style.cardList}>
+          
         {Array.isArray(currentGames) &&
           currentGames.map((game) => (
             <Card key={game.id} game={game} />
