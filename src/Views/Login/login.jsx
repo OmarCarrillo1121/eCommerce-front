2cf7fd13e2aac62bbace63e9bebca0c58a1f8db5
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-11-24 08:47:39 -0600

    Auth- Se añadio diseño original al login de usuarios

--- src/Views/Login/login.jsx
+++ src/Views/Login/login.jsx
@@ -1,86 +1,15 @@
-// import React, { useState } from "react";
-// import Style from "./login.module.css";
-// import { NavLink, useNavigate } from "react-router-dom";
-// import { useField } from "../../util/hook/form/useField";
-// import { useHandle } from "../../util/hook/common/useHandle";
-// import Register from "../register/register";
-// import Input from "../../components/input/input";
-// import logo from "../../Assets/img/icon/nav/logo.png";
-// import Button from "../../components/button/button";
-// import Middle from "./middle/middle";
-// import Services from "./services/services";
-// import { signInWithEmailAndPassword } from "firebase/auth";
-// import { auth } from "../../config/firebase-config";
-
-// export default function Login() {
-//   const navigate = useNavigate();
-//   const { handle, handleChange } = useHandle();
-//   const email = useField({ type: "email" });
-//   const password = useField({ type: "password" });
-
-//   const [error, setError] = useState();
-
-//   const submitHandler = async (event, email, password) => {
-//     //event.preventDefault();
-//     //if (!email) return alert("Ingresar email");
-//     //if (!password) return alert("Ingresar password");
-//     try {
-//       const userCredentials = await signInWithEmailAndPassword(
-//         auth,
-//         email,
-//         password
-//       );
-//       console.log(userCredentials);
-//       alert("Usuario logueado exitosamente");
-//       navigate("/");
-//     } catch (error) {
-//       setError(error.message);
-//       console.log(error);
-//     }
-//   };
-
-//   return (
-//     <form
-//       onSubmit={() => submitHandler(email.value, password.value)}
-//       autoComplete="off"
-//       className={Style.form}
-//     >
-//       <NavLink to={"/"}>
-//         <img src={logo} alt="logo" className={Style.form_logo} />
-//       </NavLink>
-//       {handle ? (
-//         <section className={Style.form_login}>
-//           <Services />
-//           <article className={Style.form_interface}>
-//             <Middle />
-//             <Input userInfo={email} name="Email Adress" width={500} />
-//             <Input userInfo={password} name="Password" width={500} />
-//             <Button onClick={handleChange} children={"Register"} />
-//             <Button
-//               type="submit"
-//               // onClick={() => submit(email.value, password.value)}
-//               children={"Login"}
-//             />
-//           </article>
-//         </section>
-//       ) : (
-//         <Register handleChange={handleChange} />
-//       )}
-//       <section className={Style.form_img}>
-//         <span className={Style.form_exit_button} onClick={() => navigate("/")}>
-//           X
-//         </span>
-//       </section>
-//     </form>
-//   );
-// }
-
-//****************************** */
-
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-//import { useAuth } from "../context/AuthContext";
-//import { Alert } from "./Alert";
+import Style from "./login.module.css";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useField } from "../../util/hook/form/useField";
+import { useHandle } from "../../util/hook/common/useHandle";
+import Register from "../register/register";
+import Input from "../../components/input/input";
+import logo from "../../Assets/img/icon/nav/logo.png";
+import Button from "../../components/button/button";
+import Middle from "./middle/middle";
+import Services from "./services/services";
+import { auth } from "../../config/firebase-config";
 import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
@@ -88,59 +17,32 @@ import {
   signInWithPopup,
   sendPasswordResetEmail,
 } from "firebase/auth";
-import { auth } from "../../config/firebase-config";
 import { authUser } from "../../redux/actions.js";
 import { useDispatch } from "react-redux";
 
 export default function Login() {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  //const { login, loginWithGoogle, resetPassword } = useAuth();
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
+  const { handle, handleChange } = useHandle();
+
+  const email = useField({ type: "email" });
+  const password = useField({ type: "password" });
+
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  // const login = async (email, password) => {
-  //   const userCredentials = await signInWithEmailAndPassword(
-  //     auth,
-  //     email,
-  //     password
-  //   );
-  //   console.log(userCredentials);
-  //   return userCredentials;
-  // };
+  const [error, setError] = useState();
 
-  const loginWithGoogle = () => {
-    const googleProvider = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleProvider);
-  };
-
-  const resetPassword = async (email) => sendPasswordResetEmail(auth, email);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
+  const loginWithGoogle = async () => {
     try {
-      const userCredentials = await signInWithEmailAndPassword(
-        auth,
-        user.email,
-        user.password
-      );
-      console.log(userCredentials);
-      const authUserInfo = userCredentials;
-      dispatch(authUser(authUserInfo));
-      alert("¡Logueado con exito!");
-      navigate("/");
+      const googleProvider = new GoogleAuthProvider();
+      return await signInWithPopup(auth, googleProvider);
     } catch (error) {
       setError(error.message);
       alert(error.message);
     }
   };
 
-  const handleChange = ({ target: { value, name } }) =>
-    setUser({ ...user, [name]: value });
+  const resetPassword = async (email) =>
+    await sendPasswordResetEmail(auth, email);
 
   const handleGoogleSignin = async () => {
     try {
@@ -155,70 +57,263 @@ export default function Login() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      );
+      console.log(userCredentials);
+      const authUserInfo = userCredentials;
+      dispatch(authUser(authUserInfo));
+      alert("¡Logueado con exito!");
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+      alert(error.message);
+    }
+  };
+
   const handleResetPassword = async (e) => {
     e.preventDefault();
-    if (!user.email) return setError("Write an email to reset password");
+    if (!email.value) return setError("Write an email to reset password");
     try {
-      await resetPassword(user.email);
+      await resetPassword(email.value);
       setError("We sent you an email. Check your inbox");
     } catch (error) {
       setError(error.message);
     }
   };
 
-  //console.log(user.email, user.password);
   console.log(error);
 
   return (
-    <div className="w-full max-w-xs m-auto">
-      {/* {error && <Alert message={error} />} */}
-
-      <form onSubmit={handleSubmit} className="Form">
-        <div className="div">
-          <label htmlFor="email" className="Email label">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={handleChange}
-            className="Email"
-            placeholder="Ingrese email..."
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="Password label">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            onChange={handleChange}
-            className="Password"
-            placeholder="*************"
-          />
-        </div>
-
-        <div className="flex items-center justify-between">
-          <button className="Sign in" type="submit">
-            Sign In
-          </button>
-          <a className="Reset password" href="#!" onClick={handleResetPassword}>
-            Forgot Password?
-          </a>
-        </div>
-      </form>
-      <button onClick={handleGoogleSignin} className="Google login">
-        Google login
-      </button>
-      <p className="Register">
-        Don't have an account?
-        <Link to="/register" className="text-blue-700 hover:text-blue-900">
-          Register
-        </Link>
-      </p>
-    </div>
+    <form autoComplete="off" className={Style.form}>
+      <NavLink to={"/"}>
+        <img src={logo} alt="logo" className={Style.form_logo} />
+      </NavLink>
+      {handle ? (
+        <section className={Style.form_login}>
+          <Services />
+          <article className={Style.form_interface}>
+            <Middle />
+            <Input userInfo={email} name="Email" width={500} />
+            <Input userInfo={password} name="Password" width={500} />
+            <Button onClick={handleChange} children={"Register"} />
+            <Button onClick={handleSubmit} children={"Login"} />
+          </article>
+        </section>
+      ) : (
+        <Register handleChange={handleChange} />
+      )}
+      <section className={Style.form_img}>
+        <span className={Style.form_exit_button} onClick={() => navigate("/")}>
+          X
+        </span>
+      </section>
+    </form>
   );
 }
+
+// export default function Login() {
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+
+//   const [user, setUser] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   const [error, setError] = useState();
+
+//   const loginWithGoogle = () => {
+//     const googleProvider = new GoogleAuthProvider();
+//     return signInWithPopup(auth, googleProvider);
+//   };
+
+//   const resetPassword = async (email) => sendPasswordResetEmail(auth, email);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setError("");
+//     try {
+//       const userCredentials = await signInWithEmailAndPassword(
+//         auth,
+//         user.email,
+//         user.password
+//       );
+//       console.log(userCredentials);
+//       const authUserInfo = userCredentials;
+//       dispatch(authUser(authUserInfo));
+//       alert("¡Logueado con exito!");
+//       navigate("/");
+//     } catch (error) {
+//       setError(error.message);
+//       alert(error.message);
+//     }
+//   };
+
+//   const handleChange = ({ target: { value, name } }) =>
+//     setUser({ ...user, [name]: value });
+
+//   const handleGoogleSignin = async () => {
+//     try {
+//       const userCredentials = await loginWithGoogle();
+//       console.log(userCredentials);
+//       const authUserInfo = userCredentials;
+//       dispatch(authUser(authUserInfo));
+//       alert("¡Logueado con exito!");
+//       navigate("/");
+//     } catch (error) {
+//       setError(error.message);
+//     }
+//   };
+
+//   const handleResetPassword = async (e) => {
+//     e.preventDefault();
+//     if (!user.email) return setError("Write an email to reset password");
+//     try {
+//       await resetPassword(user.email);
+//       setError("We sent you an email. Check your inbox");
+//     } catch (error) {
+//       setError(error.message);
+//     }
+//   };
+
+//   console.log(error);
+
+//   return (
+//     <form onSubmit={handleSubmit} autoComplete="off" className={Style.form}>
+//       <NavLink to={"/"}>
+//         <img src={logo} alt="logo" className={Style.form_logo} />
+//       </NavLink>
+//       <section className={Style.form_login}>
+//         <article className={Style.form_interface}>
+//           {/* <Middle /> */}
+//           <div className={Style.group}>
+//             <label htmlFor="email" className={Style.label}>
+//               Email
+//             </label>
+//             <input
+//               type="email"
+//               name="email"
+//               id="email"
+//               onChange={handleChange}
+//               className={Style.input}
+//               placeholder=""
+//             />
+//           </div>
+//           <div className={Style.group}>
+//             <label htmlFor="password" className={Style.label}>
+//               Password
+//             </label>
+//             <input
+//               type="password"
+//               name="password"
+//               id="password"
+//               onChange={handleChange}
+//               className={Style.input}
+//               placeholder=""
+//             />
+//           </div>
+//           <div className="flex items-center justify-between">
+//             <button className="Sign in" type="submit">
+//               Sign In
+//             </button>
+//             <a
+//               className="Reset password"
+//               href="#!"
+//               onClick={handleResetPassword}
+//             >
+//               Forgot Password?
+//             </a>
+//             <button
+//               onClick={handleGoogleSignin}
+//               className={Style.form_button}
+//               // style={{
+//               //   width: `${viewportWidth <= 800 ? 150 + "px" : width + "px"}`,
+//               // }}
+//             >
+//               Google login
+//             </button>
+//             <p className="Register">
+//               Don't have an account?
+//               <Link
+//                 to="/register"
+//                 className="text-blue-700 hover:text-blue-900"
+//               >
+//                 Register
+//               </Link>
+//             </p>
+//           </div>
+//         </article>
+//       </section>
+//       <section className={Style.form_img}>
+//         <span className={Style.form_exit_button} onClick={() => navigate("/")}>
+//           X
+//         </span>
+//       </section>
+//     </form>
+//   );
+// }
+
+// //****************************** */
+// // export default function Login() {
+
+// //   //console.log(user.email, user.password);
+
+// //   return (
+// //     <div className="w-full max-w-xs m-auto">
+// //       {/* {error && <Alert message={error} />} */}
+
+// //       <form onSubmit={handleSubmit} className="Form">
+// // <div className="div">
+// //   <label htmlFor="email" className="Email label">
+// //     Email
+// //   </label>
+// //   <input
+// //     type="email"
+// //     name="email"
+// //     id="email"
+// //     onChange={handleChange}
+// //     className="Email"
+// //     placeholder="Ingrese email..."
+// //   />
+// // </div>
+// // <div className="mb-4">
+// //   <label htmlFor="password" className="Password label">
+// //     Password
+// //   </label>
+// //   <input
+// //     type="password"
+// //     name="password"
+// //     id="password"
+// //     onChange={handleChange}
+// //     className="Password"
+// //     placeholder="*************"
+// //   />
+// // </div>
+
+// //         <div className="flex items-center justify-between">
+// //           <button className="Sign in" type="submit">
+// //             Sign In
+// //           </button>
+// //           <a className="Reset password" href="#!" onClick={handleResetPassword}>
+// //             Forgot Password?
+// //           </a>
+// //         </div>
+// //       </form>
+// //       <button onClick={handleGoogleSignin} className="Google login">
+// //         Google login
+// //       </button>
+// //       <p className="Register">
+// //         Don't have an account?
+// //         <Link to="/register" className="text-blue-700 hover:text-blue-900">
+// //           Register
+// //         </Link>
+// //       </p>
+// //     </div>
+// //   );
+// // }
--- src/Views/register/register.jsx
+++ src/Views/register/register.jsx
@@ -1,141 +1,195 @@
+import React from "react";
+import Style from "./register.module.css";
+import Input from "../../components/input/input";
+import Button from "../../components/button/button";
+import { useField } from "../../util/hook/form/useField";
+
+export default function Register({ handleChange }) {
+  // las variables de useField tienen la propiedad value y onChange, para acceder a sus valores o modificarlos, por ej: emanil.value o userName.onChange
+  const userName = useField({ type: "user" });
+  const email = useField({ type: "email" });
+  const password = useField({ type: "password" });
+  const adress = useField({ type: "adress" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert("YESS");
+    // setError("");
+    // try {
+    //   const userCredentials = await signInWithEmailAndPassword(
+    //     auth,
+    //     user.email,
+    //     user.password
+    //   );
+    //   console.log(userCredentials);
+    //   const authUserInfo = userCredentials;
+    //   dispatch(authUser(authUserInfo));
+    //   alert("¡Logueado con exito!");
+    //   navigate("/");
+    // } catch (error) {
+    //   setError(error.message);
+    //   alert(error.message);
+    // }
+  };
+
+  return (
+    <div className={Style.form_register}>
+      <h1>Register</h1>
+      <div className={Style.form_register_inp}>
+        <Input userInfo={userName} name={"User Name"} width={250} />
+        <Input userInfo={email} name={"Email Adress"} width={250} />
+        <Input userInfo={password} name={"Password"} width={250} />
+        <Input userInfo={adress} name={"Adress"} width={250} />
+        <div className={Style.form_register_button}>
+          <Button children={"Atras"} onClick={handleChange} />
+          <Button children={"Register"} onClick={handleSubmit} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/* --------- */
+
+// // import React, { useState } from "react";
+// // import Style from "./register.module.css";
+// // import Input from "../../components/input/input";
+// // import Button from "../../components/button/button";
+// // import { useField } from "../../util/hook/form/useField";
+// // import { createUserWithEmailAndPassword } from "firebase/auth";
+// // import { auth } from "../../config/firebase-config";
+// // import { useNavigate } from "react-router-dom";
+
+// // export default function Register({ handleChange }) {
+// //   // las variables de useField tienen la propiedad value y onChange, para acceder a sus valores o modificarlos, por ej: emanil.value o userName.onChange
+// //   const userName = useField({ type: "user" });
+// //   const email = useField({ type: "text" });
+// //   const password = useField({ type: "password" });
+// //   const adress = useField({ type: "adress" });
+
+// //   const [error, setError] = useState();
+// //   const navigate = useNavigate();
+
+// //   const signup = async (event, email, password) => {
+// //     //event.preventDefault();
+// //     //if (!email) return alert("Ingresar email");
+// //     //if (!password) return alert("Ingresar password");
+// //     try {
+// //       await createUserWithEmailAndPassword(auth, email, password);
+// //       alert("Usuario creado exitosamente");
+// //       navigate("/");
+// //     } catch (error) {
+// //       setError(error.message);
+// //     }
+// //   };
+
+// //   // const signup = (email, password) => {
+// //   //   alert(JSON.stringify(email, password));
+// //   // };
+
+// //   console.log(email.value, password.value);
+
+// //   return (
+// //     <div className={Style.form_register}>
+// //       <h1>Register</h1>
+// //       <div className={Style.form_register_inp}>
+// //         <Input userInfo={userName} name={"User Name"} width={250} />
+// //         <Input userInfo={email} name={"Email Adress"} width={250} />
+// //         <Input userInfo={password} name={"Password"} width={250} />
+// //         <Input userInfo={adress} name={"Adress"} width={250} />
+// //         <div className={Style.form_register_button}>
+// //           <Button children={"Atras"} onClick={handleChange} />
+// //           <Button
+// //             children={"Register"}
+// //             onClick={() => signup(email.value, password.value)}
+// //           />
+// //           {error && <p>{error}</p>}
+// //         </div>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// //************************************************** */
+
 // import React, { useState } from "react";
-// import Style from "./register.module.css";
-// import Input from "../../components/input/input";
-// import Button from "../../components/button/button";
-// import { useField } from "../../util/hook/form/useField";
+// import { Link, useNavigate } from "react-router-dom";
+// //import { useAuth } from "../context/AuthContext";
+// //import { Alert } from "./Alert";
 // import { createUserWithEmailAndPassword } from "firebase/auth";
 // import { auth } from "../../config/firebase-config";
-// import { useNavigate } from "react-router-dom";
+// import { authUser } from "../../redux/actions.js";
+// import { useDispatch } from "react-redux";
+// import Style from "./register.module.css";
 
-// export default function Register({ handleChange }) {
-//   // las variables de useField tienen la propiedad value y onChange, para acceder a sus valores o modificarlos, por ej: emanil.value o userName.onChange
-//   const userName = useField({ type: "user" });
-//   const email = useField({ type: "text" });
-//   const password = useField({ type: "password" });
-//   const adress = useField({ type: "adress" });
+// export default function Register() {
+//   //const { signup } = useAuth();
 
-//   const [error, setError] = useState();
+//   const [user, setUser] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const [error, setError] = useState("");
 //   const navigate = useNavigate();
+//   const dispatch = useDispatch();
 
-//   const signup = async (event, email, password) => {
-//     //event.preventDefault();
-//     //if (!email) return alert("Ingresar email");
-//     //if (!password) return alert("Ingresar password");
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setError("");
 //     try {
-//       await createUserWithEmailAndPassword(auth, email, password);
-//       alert("Usuario creado exitosamente");
+//       const userCredentials = await createUserWithEmailAndPassword(
+//         auth,
+//         user.email,
+//         user.password
+//       );
+//       console.log(userCredentials);
+//       const authUserInfo = userCredentials;
+//       dispatch(authUser(authUserInfo));
+//       alert("¡Usuario creado con exito!");
 //       navigate("/");
 //     } catch (error) {
 //       setError(error.message);
 //     }
 //   };
 
-//   // const signup = (email, password) => {
-//   //   alert(JSON.stringify(email, password));
-//   // };
-
-//   console.log(email.value, password.value);
-
 //   return (
-//     <div className={Style.form_register}>
-//       <h1>Register</h1>
-//       <div className={Style.form_register_inp}>
-//         <Input userInfo={userName} name={"User Name"} width={250} />
-//         <Input userInfo={email} name={"Email Adress"} width={250} />
-//         <Input userInfo={password} name={"Password"} width={250} />
-//         <Input userInfo={adress} name={"Adress"} width={250} />
-//         <div className={Style.form_register_button}>
-//           <Button children={"Atras"} onClick={handleChange} />
-//           <Button
-//             children={"Register"}
-//             onClick={() => signup(email.value, password.value)}
+//     <div className="div">
+//       {/* {error && <Alert message={error} />} */}
+
+//       <form onSubmit={handleSubmit} className={Style.form}>
+//         <div className="mb-4">
+//           <label htmlFor="email" className="email">
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             onChange={(e) => setUser({ ...user, email: e.target.value })}
+//             className="email"
+//             placeholder="Ingrese email..."
 //           />
-//           {error && <p>{error}</p>}
 //         </div>
-//       </div>
+
+//         <div className="mb-4">
+//           <label htmlFor="password" className="password">
+//             Password
+//           </label>
+//           <input
+//             type="password"
+//             onChange={(e) => setUser({ ...user, password: e.target.value })}
+//             className="Password"
+//             placeholder="*************"
+//           />
+//         </div>
+
+//         <button className="Register">Register</button>
+//       </form>
+//       <p className="Forgot">
+//         Already have an Account?
+//         <Link to="/login" className="Link login">
+//           Login
+//         </Link>
+//       </p>
 //     </div>
 //   );
 // }
-
-//************************************************** */
-
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-//import { useAuth } from "../context/AuthContext";
-//import { Alert } from "./Alert";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../config/firebase-config";
-import { authUser } from "../../redux/actions.js";
-import { useDispatch } from "react-redux";
-
-export default function Register() {
-  //const { signup } = useAuth();
-
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      const userCredentials = await createUserWithEmailAndPassword(
-        auth,
-        user.email,
-        user.password
-      );
-      console.log(userCredentials);
-      const authUserInfo = userCredentials;
-      dispatch(authUser(authUserInfo));
-      alert("¡Usuario creado con exito!");
-      navigate("/");
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  return (
-    <div className="div">
-      {/* {error && <Alert message={error} />} */}
-
-      <form onSubmit={handleSubmit} className="form">
-        <div className="mb-4">
-          <label htmlFor="email" className="email">
-            Email
-          </label>
-          <input
-            type="email"
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-            className="email"
-            placeholder="Ingrese email..."
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="password" className="password">
-            Password
-          </label>
-          <input
-            type="password"
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-            className="Password"
-            placeholder="*************"
-          />
-        </div>
-
-        <button className="Register">Register</button>
-      </form>
-      <p className="Forgot">
-        Already have an Account?
-        <Link to="/login" className="Link login">
-          Login
-        </Link>
-      </p>
-    </div>
-  );
-}
