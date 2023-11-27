6fb67a110e58cd649e42aaf2d7f91d72b317b3d5
unknown <luis901810@gmail.com>
2023-11-26 20:07:20 -0500

    subo discount y modificacion de card 2

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Style from "./Card.module.css"
 import { motion } from "framer-motion";
+import Discount from "../Discount/Discount"
 
 function Card ({ game }){
     const {id, name, image, price, discount } = game
@@ -16,11 +17,13 @@ function Card ({ game }){
             <div className = {Style.card}>
               <img className= {Style.card_game} src={image} alt={name} />
               <span className={Style.descuento}>-{discount}%</span>
+             
             </div>
             </Link>
             <div className={Style.card_price}>
                 <h2 className={Style.card_name}>{name}</h2>
-                <h2>${price}</h2>
+           
+                <Discount price ={price} porcentaje= {discount} />  
             </div>
         </motion.figure>
     )
