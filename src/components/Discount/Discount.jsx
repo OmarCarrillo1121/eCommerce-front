f6de807fad60085f48367346fb2e2aa711dd1775
patorodriguez12 <pato_rodriguez12@outlook.com>
2023-11-30 11:50:36 -0300

    frontend-pato

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -7,21 +7,26 @@ function Discount({ price, porcentaje }) {
   // Verifica si hay descuento y renderiza según la condición
   return (
     <div className={Style.discountContainer}>
-      {porcentaje !== 0 && priceDiscount !== null && ( // Asegúrate de que priceDiscount no sea null ni undefined
-        <p>
-          <span className={Style.originalPrice}>{`$${price.toFixed(2)}`}</span>{" "}
-          <span className={Style.discountedPrice}>{`$${priceDiscount.toFixed(2)}`}</span>
-        </p>
-      )}
+      {porcentaje !== 0 &&
+        priceDiscount !== null && ( // Asegúrate de que priceDiscount no sea null ni undefined
+          <p>
+            <span className={Style.originalPrice}>{`$${price.toFixed(
+              2
+            )}`}</span>{" "}
+            <span className={Style.discountedPrice}>{`$${priceDiscount.toFixed(
+              2
+            )}`}</span>
+          </p>
+        )}
       {porcentaje === 0 && ( // Mostrar solo el precio sin descuento
         <p>
-          <span className={Style.originalPriceNoDiscount}>{`$${price.toFixed(2)}`}</span>
+          <span className={Style.originalPriceNoDiscount}>{`$${price.toFixed(
+            2
+          )}`}</span>
         </p>
       )}
     </div>
   );
 }
-
-
 
 export default Discount;
